f7103a6eab472386f46da9a99b17d736d3ec750e
jvmMachado <joaovmmachado@gmail.com>
2025-11-18 22:57:53 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    expect.any(Number),
+  );
+
+  expect(responseBody.dependencies.database.open_connections).toBeDefined();
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
