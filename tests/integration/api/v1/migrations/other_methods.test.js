8aa23ee61c5cce500e67e145acd505020db6a6c9
jvmMachado <joaovmmachado@gmail.com>
2025-11-27 19:40:44 -0300

    fix `/migrations` endpoint db connection bug

---
+++ tests/integration/api/v1/migrations/other_methods.test.js
@@ -0,0 +1,16 @@
+const methods = ["PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"];
+
+test("Methods different than GET and POST should return 405", async () => {
+  for (let method of methods) {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: method,
+    });
+    expect(response.status).toBe(405);
+  }
+});
+
+test("Should contain only one open connection to the database", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const resonseBody = await response.json();
+  expect(resonseBody.dependencies.database.open_connections).toEqual(1);
+});
